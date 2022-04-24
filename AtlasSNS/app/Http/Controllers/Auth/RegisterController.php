7302717954b566f1c33f18bf77b436f2d3df54c7
kanagoto <tempesta2268@gmail.com>
2022-04-24 17:21:41 +0900

    バリデーションの適用

--- AtlasSNS/app/Http/Controllers/Auth/RegisterController.php
+++ AtlasSNS/app/Http/Controllers/Auth/RegisterController.php
@@ -48,11 +48,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
+        $validator = Validator::make($data, [
             'username' => 'required|string|max:255',
             'mail' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:4|confirmed',
         ]);
+        if($validator->fails()){
+            return redirect('/register')->withErrors($validator)->withInput();
+        }
     }
 
     /**
